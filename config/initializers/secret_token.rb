f9283577872d2cc956db38873f95f97ccf1ce460
Cristian Pereyra <cristian@redmintlabs.com>
2014-03-01 12:53:34 -0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackaton::Application.config.secret_token = '17beff35787ef5766e50084731c06d862fb3cae515654662bb2e9aba1e6af7d1046e2250b9fb15c626274855eb808259fe0802702d55154994266d19dae5a364'
